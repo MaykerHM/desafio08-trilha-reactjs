3f4dc5977c8ab0cac1119b9b09bafca1883f4a4f
Mayker <hydeki8@gmail.com>
2021-10-11 15:17:55 -0300

    RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,7 +81,8 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */
+        hasNextPage ?? <Button onClick={()=>fetchNextPage()}>{isFetchingNextPage ? "Carregar mais": "Carregando..."}</Button>}
       </Box>
     </>
   );
